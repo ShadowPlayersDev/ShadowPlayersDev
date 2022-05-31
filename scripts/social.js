102c4bafecb4f52bf91c764b804aebed7de05da4
Alex0622 <aleximora2206@gmail.com>
2022-05-31 11:38:13 -0600

    Agregar servidor de Discord y actualizar info

--- scripts/social.js
+++ scripts/social.js
@@ -7,6 +7,7 @@ const createSocial = () => {
             <a href="https://github.com/ShadowPlayersDev" class="github" target="_blank">GitHub</a>
             <a href="https://open.spotify.com/user/s1abycxsaceyrqvi43fz07kwi" class="spotify" target="_blank">Spotify</a>
             <a href="https://www.tiktok.com/@shadowplayersdev/" class="tiktok" target="_blank">TikTok</a>
+            <a href="https://discord.gg/2k8G2Fer4R" class="discord" target="_blank">Discord</a>
         </div>
     `;
 }
